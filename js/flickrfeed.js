c6deca0a603aadfaa7d19140b0a4880b7b87167b
Michael <kafej666@gmail.com>
2013-11-17 01:49:27 +0100

    Initial Version 1.0
    DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
    Version 2, December 2004
    
    Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
    
    Everyone is permitted to copy and distribute verbatim or modified
    copies of this license document, and changing it is allowed as long
    as the name is changed.
    
    DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
    
    0. You just DO WHAT THE FUCK YOU WANT TO.

---
+++ js/flickrfeed.js
@@ -0,0 +1,56 @@
+(function($){
+$.fn.flickr = function(settings){
+	//standard plugin settings
+	settings = $.extend(true, {
+		flickrbase: 'http://api.flickr.com/services/feeds/',
+		feedapi: 'photos_public.gne',
+		limit: 10,
+		options: {
+			lang: 'en-us',
+			format: 'json',
+			jsoncallback: '?'
+		},
+	}, settings);
+	
+	var url = settings.flickrbase + settings.feedapi + '?';
+	var first = true;
+	var truelimit = settings.limit - 1;
+	
+	//adding options to url
+	for(var key in settings.options){
+		if(!first)
+			url += '&';
+		url += key + '=' + settings.options[key];
+		first = false;
+	}
+	
+	//getting images from flickr
+	return $(this).each(function(){
+		var $this = $(this);
+		var template = "";
+		$.getJSON( url, function(data){
+			
+			$.each( data.items, function( i, item ) {
+				//images to thumbnails
+				var thumbnails = (item.media.m);
+
+			   	//closing card	
+				$('.close').click(function(e){
+				    $(this).parent().fadeOut(2500).remove();
+				});
+
+				// template of displayed images
+				template += '<div class="card"><div class="cardtitle">Flickr</div><div class="flickrtags">Tag: '+ settings.options.tags +'</div><div title="Zamknij" class="close"></div><a href="' + item.link + '" target="_blank">';
+				template += '<img title="' + item.title + '" src="' + thumbnails;
+				template += '" alt="'; template += item.title + '" />';
+				template += '</a></div>';
+				
+				//displaying a fixed amount of pictures.
+				if ( i === truelimit ) {
+					$this.append(template);
+				}
+			});
+		});
+	});
+};
+})(jQuery);
