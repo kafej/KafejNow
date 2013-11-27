b73236a7d96ad0dd665fcd775b8b881a776663b8
Michael <kafej666@gmail.com>
2013-11-27 03:32:29 +0100

    Version 1.2
    Ready for Cookies implementation
    ToDo card
    Really a lot of things was optimized or rewritten

--- index.php
+++ index.php
@@ -14,11 +14,22 @@
 	<script src="js/twitterfeed.js" type="text/javascript"></script>
 	<script src="js/zweatherfeed.js" type="text/javascript"></script>
 	<script src="js/jquery-ui.js" type="text/javascript"></script>
+	<script src="js/cookie.js" type="text/javascript"></script>
 	<script>
 	$(document).ready(function() {
 		// Variable for main flickr
 		var tag = $('.flickr').data('tags');
 		var page = $('.flickr').data('pages');
+		// Variable for changed flickr
+		$('#flickrtags').val(tag);
+		$('#flickrnumebrs').val(page);
+
+
+		// // set cookie
+		// $.cookie('my_cookie_name', 'value inside of it');
+
+		// // get cookie
+		// alert($.cookie('my_cookie_name'));
 
 		var searchterm = "kafej666";
 		$('#twitteruser').val(searchterm);
@@ -35,7 +46,23 @@
 		// 	    }
 		// 	});
 		// }); 
-   
+   		
+		//Set refreshing
+	    setInterval(function() {
+			// Get user tag and run flickr function
+			$('.flickr').each(function(index) {
+				$(this).attr({
+					tags: 'tag',
+					pages: 'page'
+				})
+				.flickr({
+					limit: page,
+					options: {
+						tags: tag
+					}
+				});
+			});
+	    }, 200000);
 		// More cards
 		$('#morecards').click(function (e) {
 			$('#clickedmorecards').html('Soon...');
@@ -52,15 +79,14 @@
 		// Settings option changing
 		$('#settingsmain').submit( function(e) {
 			weatherGeocode('weatherLocation','weatherList');
-			e.preventDefault()
+			e.preventDefault();
 
 			// Variable for changed flickr
 			var tag2 = $('#flickrtags').val();
 			var page2 = $('#flickrnumebrs').val();
 
-			if (tag !== tag2 && page !== page2) {
+			// if (tag !== tag2 && page !== page2) {
 				$('.flickr').each(function(index) {
-
 					$('#flickrtags').val(tag2);
 					$('#flickrnumebrs').val(page2);
 
@@ -75,10 +101,9 @@
 						}
 					});
 				});
-
-			} else{
-				return false;
-			};
+			// } else{
+				// return false;
+			// };
 		});
 		function showLocation(address,woeid) {
 			$('#weatherReport').empty();
@@ -151,14 +176,14 @@
 						}
 					},
 					error: function(data) {
-						output.innerHTML = 'An error has occurred';
+						output.innerHTML = 'An error. Please write an email to <a href="mailto:kafej666@gmail.com?subject=Mail from KafejNow" "KafejNow">KafejNow</a>';
 					}
 				});
 
 			} else {
 
 				// No search given
-				output.innerHTML = 'Please enter a location or partial address';
+				// output.innerHTML = 'Please enter a location or partial address';
 			}
 		}
 
@@ -183,8 +208,8 @@
 		    $('#time').html(now);
 		}
 		// Showing flickr cards
-		$('.card, .flickr, #settings').each(function(index) {
-			$(this).slideUp(300);
+		$('.card, #settings').each(function(index) {
+			$(this).slideUp(500);
 			$(this).delay(1500);
 			$(this).fadeIn(500);
 		});
@@ -196,24 +221,19 @@
 		$('.close2').click(function(e){
 		    $(this).parent().css({
 		    	display: 'none'
-		    });;
+		    });
 		});
-		// Get user tag and run flickr function
-		$('.flickr').each(function(index) {
-
-			$('#flickrtags').val(tag);
-			$('#flickrnumebrs').val(page);
-
-			$(this).attr({
-				tags: 'tag',
-				pages: 'page'
-			})
-			.flickr({
-				limit: page,
-				options: {
-					tags: tag
-				}
-			});
+		$('#todo').submit( function(e) {
+			e.preventDefault();
+		    if ($('#todoinput').val() !== '') {
+		        var input_value = $('#todoinput').val();
+		        $('ul').append('<li>' + input_value + '<div title="Zamknij" id="special" class="close"></div></li>');
+		    };
+		    $('#todoinput').val('');
+		});
+		$(document).on('click', '#special', function (e) {
+		    e.preventDefault();
+		    $(this).parent().remove();
 		});
 	})
 	</script>
@@ -234,12 +254,14 @@
 					<option value="4">4</option>
 					<option value="5">5</option>
 				</select>
+				<label class="settingsoption">Flickr refreshing interval: </label>
+				<input type="text" id="flickrrefresh" class="settingsactive" size="2" disabled />
 				<label>Weather city: </label>
 				<input type="text" id="weatherLocation" name="weatherLocation" size="20" />
 				<div id="weatherList"></div>
 				<label class="settingsoption">Twitter User: </label>
 				<input type="text" id="twitteruser" class="settingsactive" size="10" disabled>
-				<input type="submit" id="settingsbutton" name="settingsubmit" value="Change" />
+				<input type="submit" id="settingsbutton" name="settingsubmit" value="Show cards" />
 			</form>
 		</div>
 		<div class="card" id="zuoo">
@@ -255,7 +277,11 @@
 		<div class="card">
 			<div class="cardtitle">ToDo List</div>
 			<div title="Zamknij" class="close"></div>
-			<div class="example">a lot of :D</div>
+			<form id="todo">
+			    <label>New Task: </label>
+			    <input type="text" id="todoinput" />
+			</form>
+			<ul id="todoa"></ul>
 		</div>
 		<div class="card">
 			<div class="cardtitle">Weather</div>
